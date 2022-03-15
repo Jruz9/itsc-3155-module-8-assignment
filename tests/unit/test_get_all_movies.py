5a93e5fcf15e105f09ce6e5b14292d61b4d22d29
reygalaviz <reygalaviz26@yahoo.com>
2022-03-15 17:39:59 -0400

    Adding test for get_all_movies function

--- tests/unit/test_get_all_movies.py
+++ tests/unit/test_get_all_movies.py
@@ -1,1 +1,9 @@
-# TODO: Feature 1
+from src.repositories.movie_repository import movie_repository_singleton
+
+
+def test_get_all_movies():
+    movie_repository_singleton.create_movie("Cloverfield", "Mark Cuban",5)
+    movie_repository_singleton.create_movie( "Role Models", "Jonah Hill", 4)
+    movieList = movie_repository_singleton.get_all_movies()
+    assert "Me" == movieList[0].director, "test passed"
+    assert "Spongebob" == movieList[0].title, "test passed"
